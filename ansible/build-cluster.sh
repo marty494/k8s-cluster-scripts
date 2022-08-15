23a9797451332efb9bc8723c747d029259ea2e24
marty494 <martinjohndavey@gmail.com>
2022-08-15 19:14:11 +0100

    Version for my cluster

--- ansible/build-cluster.sh
+++ ansible/build-cluster.sh
@@ -3,6 +3,6 @@ YAML_PB_MNT="--mount type=bind,source="${PWD}/runbooks/cluster",target=/yaml"
 CFG_MNT="--mount type=bind,source="${PWD}"/ansible.cfg,target=/etc/ansible/ansible.cfg"
 VARS_MNT="--mount type=bind,source="${PWD}"/vars.yml,target=/vars.yml"
 
-docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook yaml/k8s-runbook.yaml
+sudo docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook yaml/k8s-runbook.yaml
 
 #docker run $HOSTS_MNT $YAML_PB_MNT  $CFG_MNT -ti rdomloge/ansible ansible-playbook -v yaml/resetpasswd.yaml
